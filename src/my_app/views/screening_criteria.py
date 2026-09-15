d7823926d3a4c42ae2e691325ede4e7e27a322d8
AlexCLeduc <ledualex@gmail.com>
2026-09-15 12:38:49 -0400

    format

--- src/my_app/views/screening_criteria.py
+++ src/my_app/views/screening_criteria.py
@@ -36,14 +36,7 @@ from shortcuts import (
     StandardFormMixin,
 )
 from shortcuts import breadcrumbs as bc
-from shortcuts import (
-    cached_property,
-    dataclass,
-    reverse,
-    tdt,
-    tm,
-    transaction,
-)
+from shortcuts import cached_property, dataclass, reverse, tdt, tm, transaction
 
 ParentType = L1ScreeningQuestion | L2ScreeningQuestion | Parameter
 ChildType = (
